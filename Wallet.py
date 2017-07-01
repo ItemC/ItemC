61c25f3c34cf7ebcddb0348943487ac28ee510a5
toger5 <toger5@hotmail.de>
2017-07-01 15:27:21 -0700

    wallet initilizer

--- Wallet.py
+++ Wallet.py
@@ -2,13 +2,15 @@ from transaction import *
 from Blockchain import *
 
 class Wallet:
-    publicKey = ""
-    transactions = []
-    def __init__(key):
-        publicKey = key
 
-    def get_balance():
-        transactions = Blockchain.get_unspend_transactions_for_adress(key)
+    
+    def __init__(self, key):
+        self.publicKey = key
+        self.transactions = []
+        self.bchain = Blockchain()
+
+    def get_balance(self):
+        transactions = bchain.get_unspend_transactions_for_adress(key)
 
         balance = 0
         for t in transactions:
